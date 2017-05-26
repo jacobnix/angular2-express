ca36f0c370799d95925614dabd5f192a411e9692
jacob nix <jnix@seamgen.com>
2017-05-26 09:43:02 -0700

    Removed Node_modules and dist from repository.

--- dist/server/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serverPort = 4300;
-exports.secret = "RbBQqA6uF#msRF8s7h*?@=95HUm&DgMDd6zLFn4XzWQ6dtwXSJwBX#?gL2JWf!";
-exports.length = 128;
-exports.digest = "sha256";
-//# sourceMappingURL=/Users/jacob/code/angular2-express-starter/dist/server/config.js.map
